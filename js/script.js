646df6a0590dae3585869f448bac9c1df2962cb9
Shanmus4 <s4.shanmugsundar@gmail.com>
2018-09-24 00:25:34 +0530

    Added Two Buttons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
+var colorflag=0;
 function colorFunction(){
 
+    
     var colors= document.getElementsByClassName('mainnavcc');
-    console.log(colors);
-    colors[0].style.backgroundColor='Blue';
+    var fontColorss=document.getElementsByClassName('font');
+    
+    if(colorflag==0)
+    {
+        colors[0].style.backgroundColor='#7a7676';
+        fontColorss[0].style.color='#ededed';
+        fontColorss[1].style.color='#ededed';
+        colorflag=1;
+    }
+    else
+    {
+        colors[0].style.backgroundColor='';
+        fontColorss[0].style.color='';
+        fontColorss[1].style.color='';
+        colorflag=0;
+    }
+
 }
