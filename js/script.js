a4ff7dbb4e518f9884514449f8d6a9f19a37af06
Shanmus4 <s4.shanmugsundar@gmail.com>
2018-10-02 01:03:50 +0530

    Lots of changes

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ function colorFunction(){
     
     var colors= document.getElementsByClassName('mainnavcc');
     var fontColors=document.getElementsByClassName('font');
+    var cardcolor=document.getElementsByClassName('bottom-card')
     
     if(colorflag==0)
     {
         colors[0].style.backgroundColor='#7a7676';
         fontColors[0].style.color='#ededed';
         fontColors[1].style.color='#ededed';
+        cardcolor[0].style.backgroundColor='#7a7676';
         colorflag=1;
     }
     else
@@ -17,7 +19,14 @@ function colorFunction(){
         colors[0].style.backgroundColor='#b7b7b7';
         fontColors[0].style.color='';
         fontColors[1].style.color='';
+        cardcolor[0].style.backgroundColor='#b7b7b7';
+
         colorflag=0;
     }
 
+}
+
+function deleteFunction(e){
+    var li=e.target.parentElement;
+    removeChild(li);
 }
