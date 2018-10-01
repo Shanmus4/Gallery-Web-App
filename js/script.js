3f03c8e36b6bbee92b196a6350438060fdbf4e72
Shanmus4 <s4.shanmugsundar@gmail.com>
2018-10-01 13:37:09 +0530

    Added Button Shadows, Fixed NAvbar

--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@ function colorFunction(){
 
     
     var colors= document.getElementsByClassName('mainnavcc');
-    var fontColorss=document.getElementsByClassName('font');
+    var fontColors=document.getElementsByClassName('font');
     
     if(colorflag==0)
     {
         colors[0].style.backgroundColor='#7a7676';
-        fontColorss[0].style.color='#ededed';
-        fontColorss[1].style.color='#ededed';
+        fontColors[0].style.color='#ededed';
+        fontColors[1].style.color='#ededed';
         colorflag=1;
     }
     else
     {
-        colors[0].style.backgroundColor='';
-        fontColorss[0].style.color='';
-        fontColorss[1].style.color='';
+        colors[0].style.backgroundColor='#b7b7b7';
+        fontColors[0].style.color='';
+        fontColors[1].style.color='';
         colorflag=0;
     }
 
